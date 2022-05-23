400c583d10668043f381e79fe19b98b8cf074c90
superdug <superdug@gmail.com>
2022-05-22 23:49:37 -0500

    back to zone id

--- acm.tf
+++ acm.tf
@@ -13,18 +13,12 @@ resource "aws_acm_certificate" "default" {
   }
 }
 
-data "aws_route53_zone" "selected" {
-  name         = var.domain_name
-  //private_zone = false
-}
-
-
 resource "aws_route53_record" "validation" {
   allow_overwrite = true
   name            = tolist(aws_acm_certificate.default.domain_validation_options)[0].resource_record_name
   records         = [ tolist(aws_acm_certificate.default.domain_validation_options)[0].resource_record_value ]
   type            = tolist(aws_acm_certificate.default.domain_validation_options)[0].resource_record_type
-  zone_id         = data.aws_route53_zone.selected.zone_id
+  zone_id         = var.zone_id
   //provider        = aws.account_route53
   ttl             = "60"
 }
