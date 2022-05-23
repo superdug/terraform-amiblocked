55903b500669a93097979ed659fc3a0254d480f3
superdug <superdug@gmail.com>
2022-05-22 23:53:36 -0500

    moar default

--- acm.tf
+++ acm.tf
@@ -26,7 +26,7 @@ resource "aws_route53_record" "default" {
 resource "aws_acm_certificate_validation" "default" {
   certificate_arn = "${aws_acm_certificate.default.arn}"
   validation_record_fqdns = [
-    "${aws_route53_record.validation.fqdn}",
+    "${aws_route53_record.default.fqdn}",
   ]
 }
 
