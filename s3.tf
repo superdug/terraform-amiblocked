8f048b128ba07e5171f6db96282bbc1542e0d9d0
superdug <superdug@gmail.com>
2022-05-23 01:11:24 -0500

    terraform cloud s3 refactoring

--- s3.tf
+++ s3.tf
@@ -1,8 +1,18 @@
 # S3 bucket for website.
 resource "aws_s3_bucket" "www_bucket" {
   bucket = var.bucket_name
-  acl = "public-read"
+  //acl = "public-read"
   policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
+  tags = var.common_tags
+}
+
+resource "aws_s3_bucket_acl" "www_bucket_acl" {
+  bucket = aws_s3_bucket.www_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_cors_configuration" "www_bucket_cors" {
+  bucket = aws_s3_bucket.www_bucket.bucket
 
   cors_rule {
     allowed_headers = ["Authorization", "Content-Length"]
@@ -10,14 +20,20 @@ resource "aws_s3_bucket" "www_bucket" {
     allowed_origins = ["https://www.${var.domain_name}"]
     max_age_seconds = 3000
   }
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
+resource "aws_s3_bucket_website_configuration" "www_bucket_website" {
+  bucket = aws_s3_bucket.www_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
   }
 
-  tags = var.common_tags
+  error_document {
+    key = "error.html"
+  }
 }
+
 /*
 # S3 bucket for redirecting non-www to www.
 resource "aws_s3_bucket" "root_bucket" {
