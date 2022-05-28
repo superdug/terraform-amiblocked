4274a1ae1c6c152b0e49bb616200065e7eeb58b7
Doug Smith <superdug@gmail.com>
2022-05-28 19:01:13 +0000

    initial commit

--- providers.tf
+++ providers.tf
@@ -1,6 +1,14 @@
 terraform {
   //required_version = "~> 0.14"
 
+  backend "s3" {
+    bucket         = "amiblocked.io.tfstate"
+    key            = "ecs-platform"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
--- terraform.tfvars
+++
@@ -1,6 +0,0 @@
-domain_name = "amiblocked.io"
-bucket_name = "amiblocked.io.website"
-zone_id = "Z05643233JO758YN920J2"
-common_tags = {
-  Project = "amiblocked.io"
-}
