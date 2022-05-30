da3e997396fa36f588457a5ea03830962cdd57b8
superdug <superdug@gmail.com>
2022-05-30 00:20:47 -0500

    code cleanup

--- cloudfront.tf
+++ cloudfront.tf
@@ -56,8 +56,6 @@ resource "aws_cloudfront_distribution" "root_s3_distribution" {
   enabled = true
   is_ipv6_enabled = true
 
-  //aliases = ["${var.domain_name}"]
-
   default_cache_behavior {
     allowed_methods = ["GET", "HEAD"]
     cached_methods = ["GET", "HEAD"]
